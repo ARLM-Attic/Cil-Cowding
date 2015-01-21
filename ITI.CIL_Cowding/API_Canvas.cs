d01016998e77f8884d9ba8d021af89418a14c127
Kévin Roussel <roussel@intechinfo.fr>
2015-01-21 14:55:55 +0100

    Commencement de l'API Canvas

---
+++ ITI.CIL_Cowding/API_Canvas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.CIL_Cowding
+{
+    static class API_Canvas
+    {
+
+        static void DrawLine() { 
+            
+
+
+
+        }
+
+        static void DrawCircle() { 
+            
+
+
+
+        }
+
+
+
+    }
+}
