77fa83ffa8f92fa55399f4d45f582bb56c78a842
Mika <michael.gungaram@gmail.com>
2015-01-26 13:51:01 +0100

    Finalization of Tokenizer.

--- ITI.CIL_Cowding.Tests/ITI.CIL_Cowding.Tests/Test.cs
+++ ITI.CIL_Cowding.Tests/ITI.CIL_Cowding.Tests/Test.cs
@@ -19,7 +19,7 @@ namespace ITI.CIL_Cowding.Tests
         {
             StringTokenizer st = new StringTokenizer("ldstr");
             TokenType tk = st.CurrentToken;
-            Assert.That( tk, Is.EqualTo( TokenType.Identifier ) );
+            Assert.That(tk, Is.EqualTo(TokenType.Identifier));
         }
 
         [Test]
@@ -38,16 +38,16 @@ namespace ITI.CIL_Cowding.Tests
             Assert.That(st.IsString(out s), Is.False);
             Assert.That(st.IsIdentifier(out s), Is.False);
             Assert.That(st.Match(TokenType.OpenCurly), Is.False);
-            Assert.That(st.MatchIdentifier( "b" ), Is.False);
+            Assert.That(st.MatchIdentifier("b"), Is.False);
             int i;
-            Assert.That(st.IsInteger( out i ) && i == 1454 );
-            
+            Assert.That(st.IsInteger(out i) && i == 1454);
+
             Assert.That(st.IsString(out s), Is.False);
             Assert.That(st.IsInteger(out i), Is.False);
             Assert.That(st.Match(TokenType.SemiColon), Is.False);
-            Assert.That(st.MatchIdentifier("YOYO"), Is.False );
+            Assert.That(st.MatchIdentifier("YOYO"), Is.False);
 
-            Assert.That(st.MatchIdentifier( "toto" ) );
+            Assert.That(st.MatchIdentifier("toto"));
             Assert.That(st.Match(TokenType.EndOfInput));
 
         }
@@ -57,14 +57,14 @@ namespace ITI.CIL_Cowding.Tests
         {
             string s = "\"text\"";
 
-            StringTokenizer st = new StringTokenizer( s );
+            StringTokenizer st = new StringTokenizer(s);
             TokenType tk = st.CurrentToken;
 
-            bool value = st.IsString( out s );
+            bool value = st.IsString(out s);
             string valueOfTxt = st.GetValueOfId;
 
-            Assert.That( value, Is.True );
-            Assert.That(valueOfTxt, Is.EqualTo( "text" ) );
+            Assert.That(value, Is.True);
+            Assert.That(valueOfTxt, Is.EqualTo("text"));
 
         }
 
@@ -112,47 +112,147 @@ namespace ITI.CIL_Cowding.Tests
             Assert.That(valueOfString == "coucou");
         }
 
-
-       public void full_program()
+        [Test]
+        public void full_program()
         {
+            TokenType tkt;
             string s = @"function void main () {
-	locals_init(
-		int fac
-	)
-    write;
+	            locals_init(
+		            int fac
+	            )
+                write;
 	
-	ldstr ""fin :-)"";
-	write;
+	            ldstr ""fin :-)"";
+	            write;
 	
-}
-// line 1
-// line 2
-// line 3
-function int fact(int nb) {
-	locals_init(
-		int retour_fct
-	)
-/*
-	ldarg 0;
-	ldc 1;
-	ceq;*/
-	brfalse recur;
-	ldc /*1;
-	ret;
+            }
+            // line 1
+            // line 2
+            // line 3
+            function int fact(int nb) {
+	            locals_init(
+		            int retour_fct
+	            )
+            /*
+	            ldarg 0;
+	            ldc 1;
+	            ceq;*/
+	            brfalse recur;
+	            ldc /*1;
+	            ret;
 	
 	
-	#recur
-	ldarg 0;
-	ldc 1;
-	sub;
-	call fact;
-	ldarg 0;
-	mul;
-	ret;
-*/	
-}
+	            #recur
+	            ldarg 0;
+	            ldc 1;
+	            sub;
+	            call fact;
+	            ldarg 0;
+	            mul;
+	            ret;
+            */	
+            }
 
-";
+            ";
+            StringTokenizer st = new StringTokenizer(s);
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.OpenPar));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.ClosedPar));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.OpenCurly));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.OpenPar));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.ClosedPar));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.SemiColon));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.String));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.SemiColon));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.SemiColon));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.ClosedCurly)); // End of tokens' main function.
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.OpenPar));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.ClosedPar));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.OpenCurly));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.OpenPar));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.ClosedPar));
+            st.GetNextToken();
+            tkt = st.CurrentToken;
+            Assert.That(tkt, Is.EqualTo(TokenType.Identifier));
         }
     }
 }
